1ae93ab57c785e33be9a4de5d1ab4540d19443a0
Matt Taylor <mstaveleytaylor@gmail.com>
2021-06-24 22:33:12 +0100

    rust uutils tmp

--- scripts/cargo-inject-patches.py
+++ scripts/cargo-inject-patches.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-import argparse, os, subprocess, pathlib
+import argparse, os, subprocess, pathlib, sys, re
 
 patched_libs = {
-	'libc': '0.2.93',
+	'libc': '0.2.97',
 	'num_cpus': '1.13.0',
 	'users': '0.11.0',
+	'socket2': '0.4.0',
+	'nix': '0.21.0',
+	'getrandom': '0.2.3',
+	'filetime': '0.2.14',
+	'mio': '0.7.13',
 }
 
 parser = argparse.ArgumentParser(description='Inject patched Rust libraries into Cargo lockfiles')
@@ -28,7 +33,16 @@ for lib, version in patched_libs.items():
 	]
 
 	output = subprocess.run(cmd, capture_output=True)
-	if 'did not match any packages' in str(output.stderr):
-		print(f'cargo-inject-patches: Injecting {lib} v{version} failed, patch not used')
-	else:
+	expected = [
+		f'Updating {lib} v{version} \\(\\S+\\) -> v{version}',
+		f'Adding {lib} v{version} \\(\\S+\\)'
+	]
+
+	if any(re.search(rg, str(output.stderr)) is not None for rg in expected):
 		print(f'cargo-inject-patches: Injected {lib} v{version}')
+	elif 'did not match any packages' in str(output.stderr):
+		print(f'cargo-inject-patches: Didn\'t inject {lib} v{version} (crate not used in dependency graph)')
+	else:
+		print(f'cargo-inject-patches: Injecting {lib} v{version} failed (didn\'t find expected strings):')
+		sys.stdout.buffer.write(output.stderr)
+		exit(1)
